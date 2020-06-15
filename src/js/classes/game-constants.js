78bf5a50e5a392791ccb68d2584682f02fdd56ec
Herson Salinas <hersonhn@gmail.com>
2020-06-15 17:42:07 -0600

    visual changes for pills

--- src/js/classes/game-constants.js
+++ src/js/classes/game-constants.js
@@ -1,7 +1,7 @@
 
 export const COLORS = [
   { name: 'blue',    color: '#001a6b', background: '#0066cc' },
-  { name: 'crimson', color: 'crimson', background: '#ff8f94' },
+  { name: 'crimson', color: '#a20016', background: 'crimson' },
   { name: 'green',   color: '#006900', background: '#00991f' },
   { name: 'orange',  color: '#8e2f00', background: '#ff8b14' },
   { name: 'pink',    color: '#600099', background: '#cc33ff' },
@@ -9,12 +9,12 @@ export const COLORS = [
 ];
 
 export const SYMBOLS = [
-  { name: 'clover', icon: '♣', emoji: '🍀' },
-  { name: 'flower', icon: '✿', emoji: '🌸' },
-  { name: 'heart',  icon: '♥', emoji: '💖' },
-  { name: 'music',  icon: '♫', emoji: '🎶' },
-  { name: 'spade',  icon: '♠', emoji: '🗡' },
-  { name: 'star',   icon: '★', emoji: '🌟' },
+  { name: 'diamond', icon: '❖', emoji: '💎' },
+  { name: 'flower',  icon: '✿', emoji: '🌸' },
+  { name: 'heart',   icon: '♥', emoji: '💖' },
+  { name: 'music',   icon: '♫', emoji: '🎶' },
+  { name: 'spade',   icon: '♠', emoji: '🗡' },
+  { name: 'star',    icon: '★', emoji: '🌟' },
 ];
 
 export const REAPPEAR_DELAY = 500;
